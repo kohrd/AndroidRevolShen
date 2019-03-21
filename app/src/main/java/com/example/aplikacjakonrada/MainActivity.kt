6624a4a49febf762b578df2d4e4cc591a46e6fda
kohrd <konrad.janeczek@protonmail.com>
2019-03-21 15:11:11 +0100

    3

--- app/src/main/java/com/example/aplikacjakonrada/MainActivity.kt
+++ app/src/main/java/com/example/aplikacjakonrada/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.aplikacjakonrada
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +11,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        button2.setT
+
+//        R.id.mainLayout dostep po klasie R i po id
+        buttonYes.setOnClickListener{
+            buttonYes.setText("Działa");
+            buttonYes.setBackgroundColor(Color.BLUE);
+            var message = Toast.makeText(applicationContext, "Dziękuję!", Toast.LENGTH_SHORT); // chmurkla z informacją
+            message.show();
+        }
+
+        buttonNo.setOnClickListener{
+            buttonYes.setText("Tu Cię mam!")
+            buttonYes.setBackgroundColor(Color.RED);
+            buttonNo.setText("teraz nie działa")
+            buttonNo.setBackgroundColor(Color.YELLOW);
+            var message = Toast.makeText(applicationContext, "Kocham słonie!!", Toast.LENGTH_SHORT);
+            message.show();
+        }
+
     }
 }
